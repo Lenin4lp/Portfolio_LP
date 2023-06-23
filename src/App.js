4cf454778d6992b70593b90ddf74049f073da0b2
Lenin Patino <leninesteban98@outlook.com>
2023-06-22 22:10:18 -0500

    Correcting an import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, lazy, Suspense, useCallback } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar.js";
 import Home from "./components/Home.js";
